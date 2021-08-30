c540f4601ce510789966e76903d220e18511994e
gyorkeattila <71698318+gyorkeattila@users.noreply.github.com>
2021-08-30 19:01:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from sqlite3.dbapi2 import Cursor
 from typing import AsyncIterator, DefaultDict
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
+
+import sqlite3
 
 intents = discord.Intents.all() 
 client = commands.Bot(command_prefix='=', intents = intents)
@@ -9,6 +12,16 @@ client = commands.Bot(command_prefix='=', intents = intents)
 
 @client.event
 async def on_ready():
+    db = sqlite3.connect('bot.sqlite')
+    cursor = db.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS warn(
+        id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        server_id TEXT, 
+        user_id TEXT,
+        amount INTEGER
+        )
+        ''')
     print('Szia Uram')
 
 #ban 
@@ -66,11 +79,36 @@ async def unban_error(ctx,error):
 async def spoderman(ctx):
     await ctx.channel.send("https://media.discordapp.net/attachments/797151894275489802/875437827574366218/giphy.gif")
 
+#peace
+@client.command()
+async def peace(ctx):
+    await ctx.channel.send("https://c.tenor.com/DnY4rvDpLDwAAAAd/nileseyy-niles-disappear.gif")
 
 #mlem
 @client.command()
 async def mlem(ctx):
     await ctx.channel.send("https://media.discordapp.net/attachments/861870461700014091/875830569894834186/20210813_215749.gif")
+
+#warn
+@client.command()
+@has_permissions(administrator=True)
+async def warn(ctx,member:discord.Member):
+    db = sqlite3.connect('bot.sqlite')
+    cursor = db.cursor()
+    cursor.execute(f"SELECT amount FROM warn WHERE server_id = {ctx.guild.id} AND user_id = {member.id}")
+    result = cursor.fetchone()
+    if result is None:
+        sql = ("INSERT INTO warn (server_id, user_id, amount) VALUES (?, ?, ?)")
+        val = (ctx.guild.id, member.id, '1')
+        cursor.execute(sql, val)
+        db.commit()
+    elif result is not None:
+        sql = ("INSERT INTO warn (server_id, user_id, amount) VALUES (?, ?, ?)")
+        val = (ctx.guild.id, member.id, '1')
+        cursor.execute(sql, val)
+        db.commit()
+    cursor.close()
+    db.close()
 
 
 
