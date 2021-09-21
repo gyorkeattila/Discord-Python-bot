76811914062655acfae9bbb0d04d96e84ee30dfa
gyorkeattila <71698318+gyorkeattila@users.noreply.github.com>
2021-09-21 20:36:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
 import sqlite3
+import datetime
+
+
+import ffmpeg
+from discord import FFmpegPCMAudio
+
+from discord import voice_client
+from discord.utils import get
+
+
 
 intents = discord.Intents.all() 
 client = commands.Bot(command_prefix='=', intents = intents)
@@ -22,6 +32,8 @@ async def on_ready():
         amount INTEGER
         )
         ''')
+    global indulas
+    indulas = datetime.datetime.now()
     print('Szia Uram')
 
 #ban 
@@ -116,6 +128,50 @@ async def ping(ctx):
         embedVar = discord.Embed(title="Bot válaszideje")
         embedVar.add_field(name="Válaszidő", value=f'{round(client.latency * 1000)}ms', inline=False)
         await ctx.channel.send(embed=embedVar)
+
+
+#uptime
+@client.command()
+async def uptime(ctx):
+    now = datetime.datetime.now()
+    uptime = indulas - now
+    uptime = datetime.timedelta.strftime(uptime, '%d nap %H óra %M perc %S másodperc')
+    embedVar = discord.Embed(title="Bot futási ideje")
+    embedVar.add_field(name="Idő", value = uptime, inline=False)
+    await ctx.channel.send(embed=embedVar)
+
+#langos
+@client.command()
+async def langos(ctx):
+    await ctx.channel.send("https://media.discordapp.net/attachments/754088236338642964/822194317257867334/image0-33.gif")
+
+
+#zene
+@client.command()
+async def zene(ctx):
+    user = ctx.author.voice
+    if user is not None:
+        channel = ctx.author.voice.channel
+        voice = await channel.connect()
+        voice.play(discord.FFmpegPCMAudio(executable="C:/Users/ati42/Desktop/ffmpeg-2021-09-01-git-c500dc7cca-full_build/bin/ffmpeg.exe", source=r"C:\Users\ati42\Desktop\Wenseron botja\friday.mp3"))
+    else:
+        await ctx.send("Nem vagy bent egy hangcsatornában sem")
+
+
+#psg
+@client.command()
+async def psg(ctx):
+    await ctx.channel.send("https://media.discordapp.net/attachments/670660610438070313/886837043890638848/received_4313099998758017.jpeg?width=654&height=676")
+
+#avatar
+@client.command(aliases=['av'])
+async def avatar(ctx,user:discord.Member=None):
+    if not user:
+        user = ctx.message.author
+    embed = discord.Embed()
+    embed.set_image(url=user.avatar.url)
+    embed.set_footer(text=f"Avatar from {user}")
+    await ctx.channel.send(embed = embed)
 
 
 client.run('Discord Token')
